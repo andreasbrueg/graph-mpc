f1cc5f453e75031c243bba9eea77b5be65b78242
brunnauer <simon.brunnauer@proton.me>
2025-05-02 16:27:30 +0200

    Add networking to testing infrastructure

---
+++ setup/setup.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <boost/program_options.hpp>
+#include <cstdint>
+#include <cstring>
+#include <fstream>
+#include <iostream>
+#include <nlohmann/json.hpp>
+
+#include "../src/io/netmp.h"
+#include "utils.h"
+
+namespace bpo = boost::program_options;
+using json = nlohmann::json;
+
+namespace setup {
+bpo::options_description programOptions();
+bpo::variables_map parseOptions(bpo::options_description &cmdline, bpo::options_description &prog_opts, int argc, char *argv[]);
+void setupExecution(const bpo::variables_map &opts, size_t &pid, size_t &repeat, size_t &threads, std::shared_ptr<io::NetIOMP> &network, uint64_t *seeds_h,
+                    uint64_t *seeds_l, bool &save_output, std::string &save_file);
+}  // namespace setup
---
+++ tests/test_shuffle/test_shuffle.cpp
@@ -0,0 +1,121 @@
+#include "../../setup/setup.h"
+#include "../../src/shuffle.h"
+
+void test_shuffle(const bpo::variables_map &opts) {
+    auto vec_size = opts["vec-size"].as<size_t>();
+
+    size_t pid, repeat, threads;
+    std::shared_ptr<io::NetIOMP> network = nullptr;
+    uint64_t seeds_h[5];
+    uint64_t seeds_l[5];
+    json output_data;
+    bool save_output;
+    std::string save_file;
+
+    setup::setupExecution(opts, pid, repeat, threads, network, seeds_h, seeds_l, save_output, save_file);
+    output_data["details"] = {{"pid", pid}, {"threads", threads}, {"seeds_h", seeds_h}, {"seeds_l", seeds_l}, {"repeat", repeat}, {"vec-size", vec_size}};
+    output_data["benchmarks_pre"] = json::array();
+    output_data["benchmarks"] = json::array();
+
+    std::cout << "--- Details ---\n";
+    for (const auto &[key, value] : output_data["details"].items()) {
+        std::cout << key << ": " << value << "\n";
+    }
+    std::cout << std::endl;
+
+    std::vector<std::vector<Row>> input_vectors(3);
+    input_vectors[0].resize(vec_size);
+    input_vectors[1].resize(vec_size);
+    input_vectors[2].resize(vec_size);
+
+    assert(input_vectors.size() == 3);
+    for (size_t i = 0; i < 3; i++) assert(input_vectors[i].size() == vec_size);
+    // Set first vector to 0,1,...
+    for (size_t i = 0; i < vec_size; i++) input_vectors[0][i] = i;
+    // Set second vector to 0,1,...
+    for (size_t i = 0; i < vec_size; i++) input_vectors[1][i] = i;
+    // Set third vector to 0,2,4,6,...
+    for (size_t i = 0; i < vec_size; i++) input_vectors[2][i] = i << 1;
+
+    Party party = (pid == 0) ? P0 : ((pid == 1) ? P1 : D);
+    RandomGenerators rngs(seeds_h, seeds_l);
+    Shuffle shuffle(party, vec_size, 1, rngs, network);
+    shuffle.set_input(input_vectors[0]);
+
+    for (size_t r = 0; r < repeat; ++r) {
+        std::cout << "--- Repetition " << r + 1 << " ---" << std::endl;
+
+        StatsPoint start_pre(*network);
+        shuffle.run_offline();
+        StatsPoint end_pre(*network);
+        network->sync();
+
+        auto rbench_pre = end_pre - start_pre;
+        output_data["benchmarks_pre"].push_back(rbench_pre);
+        size_t bytes_sent_pre = 0;
+        for (const auto &val : rbench_pre["communication"]) {
+            bytes_sent_pre += val.get<int64_t>();
+        }
+        std::cout << "setup time: " << rbench_pre["time"] << " ms" << std::endl;
+        std::cout << "setup sent: " << bytes_sent_pre << " bytes" << std::endl;
+
+        StatsPoint start(*network);
+        shuffle.run_online();
+        /* Run online */
+        StatsPoint end(*network);
+        std::vector<Row> res = shuffle.result();
+
+        std::cout << "Result of shuffle: ";
+        for (int i = 0; i < res.size() - 1; ++i) {
+            std::cout << res[i] << ", ";
+        }
+        std::cout << res[res.size() - 1] << std::endl;
+
+        auto rbench = end - start;
+        output_data["benchmarks"].push_back(rbench);
+
+        size_t bytes_sent = 0;
+        for (const auto &val : rbench["communication"]) {
+            bytes_sent += val.get<int64_t>();
+        }
+
+        std::cout << "time: " << rbench["time"] << " ms" << std::endl;
+        std::cout << "sent: " << bytes_sent << " bytes" << std::endl;
+
+        std::cout << std::endl;
+    }
+
+    output_data["stats"] = {{"peak_virtual_memory", peakVirtualMemory()}, {"peak_resident_set_size", peakResidentSetSize()}};
+
+    std::cout << "--- Overall Statistics ---\n";
+    for (const auto &[key, value] : output_data["stats"].items()) {
+        std::cout << key << ": " << value << "\n";
+    }
+    std::cout << std::endl;
+
+    if (save_output) {
+        saveJson(output_data, save_file);
+    }
+    exit(0);
+}
+
+int main(int argc, char **argv) {
+    auto prog_opts(setup::programOptions());
+
+    bpo::options_description cmdline("Benchmark a simple test for shuffling and unshuffling");
+    cmdline.add(prog_opts);
+
+    cmdline.add_options()("config,c", bpo::value<std::string>(), "configuration file for easy specification of cmd line arguments")("help,h",
+                                                                                                                                    "produce help message");
+
+    bpo::variables_map opts = setup::parseOptions(cmdline, prog_opts, argc, argv);
+
+    try {
+        test_shuffle(opts);
+    } catch (const std::exception &ex) {
+        std::cerr << ex.what() << "\nFatal error" << std::endl;
+        return 1;
+    }
+
+    return 0;
+}

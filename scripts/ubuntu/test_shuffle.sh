fb75ce5773c5ba4e997ae0b7c953c6379d036fa0
brunnauer <simon.brunnauer@proton.me>
2025-05-03 14:39:50 +0200

    Revert "Change Github workflow to run test in three different tmux sessions"
    This reverts commit 92791e5a3edf8e337c8867ff8cc4beaee7453d92.

--- scripts/ubuntu/test_shuffle.sh
+++ scripts/ubuntu/test_shuffle.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
 
-tmux new-session -d -s session1 'cd ./build/tests/test_shuffle && ./test_shuffle -p 0 --localhost'
-tmux new-session -d -s session1 'cd ./build/tests/test_shuffle && ./test_shuffle -p 1 --localhost'
-tmux new-session -d -s session1 'cd ./build/tests/test_shuffle && ./test_shuffle -p 2 --localhost'
+cd ./build/tests/test_shuffle && (./test_shuffle -p 0 --localhost & ./test_shuffle -p 1 --localhost & ./test_shuffle -p 2 --localhost)

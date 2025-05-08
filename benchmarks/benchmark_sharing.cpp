ab5e2924b1009f0896978d65f780fffa717fbab5
brunnauer <simon.brunnauer@proton.me>
2025-05-08 15:45:50 +0200

    Add benchmarks for sharing

---
+++ benchmarks/benchmark_sharing.cpp
@@ -0,0 +1,95 @@
+#include "../setup/setup.h"
+#include "../src/sharing.h"
+#include "../src/shuffle.h"
+
+void benchmark(const bpo::variables_map &opts) {
+    auto vec_size = opts["vec-size"].as<size_t>();
+
+    size_t pid, nP, repeat, threads;
+    std::shared_ptr<io::NetIOMP> network = nullptr;
+    uint64_t seeds_h[5];
+    uint64_t seeds_l[5];
+    json output_data;
+    bool save_output;
+    std::string save_file;
+
+    setup::setupExecution(opts, pid, nP, repeat, threads, network, seeds_h, seeds_l, save_output, save_file);
+    output_data["details"] = {{"pid", pid},         {"num_parties", nP}, {"threads", threads},  {"seeds_h", seeds_h},
+                              {"seeds_l", seeds_l}, {"repeat", repeat},  {"vec-size", vec_size}};
+    output_data["benchmarks"] = json::array();
+
+    std::cout << "--- Details ---\n";
+    for (const auto &[key, value] : output_data["details"].items()) {
+        std::cout << key << ": " << value << "\n";
+    }
+    std::cout << std::endl;
+
+    RandomGenerators rngs(seeds_h, seeds_l);
+
+    std::vector<Row> share(vec_size);
+    std::vector<Row> input_table(vec_size);
+
+    for (size_t i = 0; i < vec_size; ++i) {
+        input_table[i] = i;
+    }
+
+    Party partner = (pid == P0 ? P1 : P0);
+
+    StatsPoint start(*network);
+    if (pid == P0) {
+        Share::random_share_secret_vec_send(partner, rngs, *network, share, input_table);
+    } else if (pid == P1) {
+        Share::random_share_secret_vec_recv(partner, *network, share);
+    }
+
+    StatsPoint end(*network);
+    network->sync();
+
+    auto rbench = end - start;
+    output_data["benchmarks"].push_back(rbench);
+
+    size_t bytes_sent = 0;
+    for (const auto &val : rbench["communication"]) {
+        bytes_sent += val.get<int64_t>();
+    }
+
+    std::cout << "time: " << rbench["time"] << " ms" << std::endl;
+    std::cout << "sent: " << bytes_sent << " bytes" << std::endl;
+
+    std::cout << std::endl;
+
+    output_data["stats"] = {{"peak_virtual_memory", peakVirtualMemory()}, {"peak_resident_set_size", peakResidentSetSize()}};
+
+    std::cout << "--- Overall Statistics ---\n";
+    for (const auto &[key, value] : output_data["stats"].items()) {
+        std::cout << key << ": " << value << "\n";
+    }
+    std::cout << std::endl;
+
+    if (save_output) {
+        saveJson(output_data, save_file);
+    }
+
+    exit(0);
+}
+
+int main(int argc, char **argv) {
+    auto prog_opts(setup::programOptions());
+
+    bpo::options_description cmdline("Benchmark a simple test for secret sharing a vector");
+    cmdline.add(prog_opts);
+
+    cmdline.add_options()("config,c", bpo::value<std::string>(), "configuration file for easy specification of cmd line arguments")("help,h",
+                                                                                                                                    "produce help message");
+
+    bpo::variables_map opts = setup::parseOptions(cmdline, prog_opts, argc, argv);
+
+    try {
+        benchmark(opts);
+    } catch (const std::exception &ex) {
+        std::cerr << ex.what() << "\nFatal error" << std::endl;
+        return 1;
+    }
+
+    return 0;
+}

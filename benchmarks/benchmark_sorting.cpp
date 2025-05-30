48ae4d4e367dbfceeb79004531d99062093aa498
brunnauer <simon.brunnauer@proton.me>
2025-05-30 18:28:16 +0200

    Add benchmarks for sorting

---
+++ benchmarks/benchmark_sorting.cpp
@@ -0,0 +1,125 @@
+#include <cassert>
+#include <random>
+
+#include "../setup/setup.h"
+#include "../src/protocol/sorting.h"
+#include "../src/utils/perm.h"
+
+void benchmark(const bpo::variables_map &opts) {
+    std::cout << "------ test_sort ------" << std::endl << std::endl;
+    auto vec_size = opts["vec-size"].as<size_t>();
+
+    size_t pid, nP, repeat, threads, shuffle_num, nodes;
+    std::shared_ptr<io::NetIOMP> network = nullptr;
+    uint64_t seeds_h[7];
+    uint64_t seeds_l[7];
+    json output_data;
+    bool save_output;
+    std::string save_file;
+
+    setup::setupExecution(opts, pid, nP, repeat, threads, shuffle_num, nodes, network, seeds_h, seeds_l, save_output, save_file);
+    output_data["details"] = {{"pid", pid},         {"num_parties", nP}, {"threads", threads},  {"seeds_h", seeds_h},
+                              {"seeds_l", seeds_l}, {"repeat", repeat},  {"vec-size", vec_size}};
+
+    std::cout << "--- Details ---\n";
+    for (const auto &[key, value] : output_data["details"].items()) {
+        std::cout << key << ": " << value << "\n";
+    }
+    std::cout << std::endl;
+
+    /* Setting up the input vector */
+    std::vector<Row> input_vector(vec_size);
+
+    for (size_t i = 0; i < vec_size; i++) {
+        input_vector[i] = rand() % vec_size;
+    }
+
+    Party party = (pid == 0) ? P0 : ((pid == 1) ? P1 : D);
+    RandomGenerators rngs(seeds_h, seeds_l);
+    ProtocolConfig conf(party, rngs, network, vec_size, 1000000);
+
+    std::vector<std::vector<Row>> bits(sizeof(Row) * 8);
+    std::vector<std::vector<Row>> bit_shares(sizeof(Row) * 8);
+
+    for (size_t i = 0; i < sizeof(Row) * 8; ++i) {
+        bits[i].resize(vec_size);
+        bit_shares[i].resize(vec_size);
+        for (size_t j = 0; j < vec_size; ++j) {
+            bits[i][j] = (input_vector[j] & (1 << i)) >> i;
+        }
+    }
+
+    for (size_t i = 0; i < bit_shares.size(); ++i) {
+        bit_shares[i] = share::random_share_secret_vec_2P(party, rngs, network, bits[i]);
+    }
+
+    for (size_t r = 0; r < repeat; ++r) {
+        std::cout << "--- Repetition " << r + 1 << " ---" << std::endl;
+
+        StatsPoint start_pre(*network);
+        auto sort_preproc = sort::get_sort_preprocess(conf, bit_shares.size());
+        StatsPoint end_pre(*network);
+        network->sync();
+
+        auto rbench_pre = end_pre - start_pre;
+        output_data["benchmarks_pre"].push_back(rbench_pre);
+        size_t bytes_sent_pre = 0;
+        for (const auto &val : rbench_pre["communication"]) {
+            bytes_sent_pre += val.get<int64_t>();
+        }
+        std::cout << "setup time: " << rbench_pre["time"] << " ms" << std::endl;
+        std::cout << "setup sent: " << bytes_sent_pre << " bytes" << std::endl;
+
+        StatsPoint start(*network);
+        auto sort_share = sort::get_sort_evaluate(conf, bit_shares, sort_preproc);
+        StatsPoint end(*network);
+
+        auto rbench = end - start;
+        output_data["benchmarks"].push_back(rbench);
+
+        size_t bytes_sent = 0;
+        for (const auto &val : rbench["communication"]) {
+            bytes_sent += val.get<int64_t>();
+        }
+
+        std::cout << "online time: " << rbench["time"] << " ms" << std::endl;
+        std::cout << "online sent: " << bytes_sent << " bytes" << std::endl;
+
+        std::cout << std::endl;
+    }
+
+    output_data["stats"] = {{"peak_virtual_memory", peakVirtualMemory()}, {"peak_resident_set_size", peakResidentSetSize()}};
+
+    std::cout << "--- Overall Statistics ---\n";
+    for (const auto &[key, value] : output_data["stats"].items()) {
+        std::cout << key << ": " << value << "\n";
+    }
+    std::cout << std::endl;
+
+    if (save_output) {
+        saveJson(output_data, save_file);
+    }
+
+    exit(0);
+}
+
+int main(int argc, char **argv) {
+    auto prog_opts(setup::programOptions());
+
+    bpo::options_description cmdline("Benchmark the sorting protocol.");
+    cmdline.add(prog_opts);
+
+    cmdline.add_options()("config,c", bpo::value<std::string>(), "configuration file for easy specification of cmd line arguments")("help,h",
+                                                                                                                                    "produce help message");
+
+    bpo::variables_map opts = setup::parseOptions(cmdline, prog_opts, argc, argv);
+
+    try {
+        benchmark(opts);
+    } catch (const std::exception &ex) {
+        std::cerr << ex.what() << "\nFatal error" << std::endl;
+        return 1;
+    }
+
+    return 0;
+}
